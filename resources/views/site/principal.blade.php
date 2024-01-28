83b536dfac2447786ef533602e8eeeb4018a173c
Sahra17 <sahra.flohr@catolicasc.org.br>
2024-01-27 22:21:59 -0300

    Primeiro commit

---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,8 @@
+<h3>Principal (view)</h3>
+<p>Olá mundo - Hello world</p>
+
+<ul>
+    <li><a href="{{ route('site.index') }}">Principal</a></li>
+    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+    <li><a href="{{ route('site.contato') }}">Contato</a></li>
+</ul>
