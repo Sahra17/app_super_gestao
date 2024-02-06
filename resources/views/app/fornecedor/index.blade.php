b8e26580f55de47da86fd4b08000905d820f04d0
Sahra17 <sahra.flohr@catolicasc.org.br>
2024-02-06 20:38:56 -0300

    Blade - if/else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,16 +1,14 @@
 <h3>Fornecedores</h3>
 
-{{ 'Texto de teste'}}
-<?= 'Texto de teste' ?>
-
-{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
-
 @php
-    // Para comentários de uma linha
 
-    /* 
-        Para comentários de multiplas linhas
-    */
-
-    echo 'Texto de teste';
+ 
 @endphp
+
+@if(count($fornnecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
