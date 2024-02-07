2b68be4bec95a9993c51f5cb33d3b3dbe3e7d6ba
Sahra17 <sahra.flohr@catolicasc.org.br>
2024-02-06 21:28:44 -0300

    Blade - unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,14 +1,19 @@
 <h3>Fornecedores</h3>
-
+<br />
 @php
 
  
 @endphp
 
-@if(count($fornnecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
+{{-- @dd($fornecedores) --}}
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br />
+Status: {{ $fornecedores[0]['status'] }}
+<br />
+@if($fornecedores[0]['status'] == 'S') 
+    'Fornecedor ativo'
 @endif
+<br />
+@unless($fornecedores[0]['status'] == 'N') <!-- Se o retorno da condição for falso -->
+    'Fornecedor ativo'
+@endunless
