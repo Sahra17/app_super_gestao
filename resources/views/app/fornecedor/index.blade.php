9ec92d18c94d89234083f32aa9b99a91fa7f1749
Sahra17 <sahra.flohr@catolicasc.org.br>
2024-02-08 21:27:44 -0300

    Blade - isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -5,15 +5,12 @@
  
 @endphp
 
-{{-- @dd($fornecedores) --}}
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br />
-Status: {{ $fornecedores[0]['status'] }}
-<br />
-@if($fornecedores[0]['status'] == 'S') 
-    'Fornecedor ativo'
-@endif
-<br />
-@unless($fornecedores[0]['status'] == 'N') <!-- Se o retorno da condição for falso -->
-    'Fornecedor ativo'
-@endunless
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    <br />
+    Status: {{ $fornecedores[1]['status'] }}
+    <br />
+    @isset($fornecedores[1]['cnpj'])
+        CNPJ: {{ $fornecedores[1]['cnpj'] }}
+    @endisset
+@endisset
